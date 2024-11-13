c0769b8a49e15eb0abc749a33c029e2606bf3bf3
Richard Rivera <RRivera5@student.fullsail.edu>
2024-11-13 11:10:04 -0500

    implemented button factory class

--- SWE_App/ButtonFactory.cpp
+++ SWE_App/ButtonFactory.cpp
@@ -1,1 +1,64 @@
 #include "ButtonFactory.h"
+
+// Generic button creation method
+wxButton* ButtonFactory::CreateButton(wxWindow* parent, wxWindowID id, const wxString& label, wxSize size)
+{
+    return new wxButton(parent, id, label, wxDefaultPosition, size, 0);
+}
+
+// Specific button creation methods
+wxButton* ButtonFactory::CreateNumberButton(wxWindow* parent, int number)
+{
+    return CreateButton(parent, wxWindowID(1000 + number), std::to_string(number), wxSize(50, 50));
+}
+
+wxButton* ButtonFactory::CreateAddButton(wxWindow* parent)
+{
+    return CreateButton(parent, wxWindowID(1010), "+", wxSize(50, 50));
+}
+
+wxButton* ButtonFactory::CreateSubtractButton(wxWindow* parent)
+{
+    return CreateButton(parent, wxWindowID(1011), "-", wxSize(50, 50));
+}
+
+wxButton* ButtonFactory::CreateMultiplyButton(wxWindow* parent)
+{
+    return CreateButton(parent, wxWindowID(1012), "*", wxSize(50, 50));
+}
+
+wxButton* ButtonFactory::CreateDivideButton(wxWindow* parent)
+{
+    return CreateButton(parent, wxWindowID(1013), "/", wxSize(50, 50));
+}
+
+wxButton* ButtonFactory::CreateModuloButton(wxWindow* parent)
+{
+    return CreateButton(parent, wxWindowID(1014), "%", wxSize(50, 50));
+}
+
+wxButton* ButtonFactory::CreateEqualsButton(wxWindow* parent)
+{
+    return CreateButton(parent, wxWindowID(1015), "=", wxSize(50, 50));
+}
+
+wxButton* ButtonFactory::CreateClearButton(wxWindow* parent)
+{
+    return CreateButton(parent, wxWindowID(1016), "C", wxSize(50, 50));
+}
+
+wxButton* ButtonFactory::CreateBackspaceButton(wxWindow* parent)
+{
+    return CreateButton(parent, wxWindowID(1017), "Del", wxSize(50, 50));
+}
+
+wxButton* ButtonFactory::CreateDecimalButton(wxWindow* parent)
+{
+    return CreateButton(parent, wxWindowID(1018), ".", wxSize(50, 50));
+}
+
+wxButton* ButtonFactory::CreateUnaryOperationButton(wxWindow* parent, const wxString& label)
+{
+    int id = (label == "sin") ? 1020 : (label == "cos") ? 1021 : 1022;
+    return CreateButton(parent, wxWindowID(id), label, wxSize(50, 50));
+}
--- SWE_App/ButtonFactory.h
+++ SWE_App/ButtonFactory.h
@@ -1,5 +1,23 @@
 #pragma once
+#include <wx/wx.h>
+
 class ButtonFactory
 {
+public:
+    // Generic button creation method
+    static wxButton* CreateButton(wxWindow* parent, wxWindowID id, const wxString& label, wxSize size = wxDefaultSize);
+
+    // Specific button creation methods
+    static wxButton* CreateNumberButton(wxWindow* parent, int number);
+    static wxButton* CreateAddButton(wxWindow* parent);
+    static wxButton* CreateSubtractButton(wxWindow* parent);
+    static wxButton* CreateMultiplyButton(wxWindow* parent);
+    static wxButton* CreateDivideButton(wxWindow* parent);
+    static wxButton* CreateModuloButton(wxWindow* parent);
+    static wxButton* CreateEqualsButton(wxWindow* parent);
+    static wxButton* CreateClearButton(wxWindow* parent);
+    static wxButton* CreateBackspaceButton(wxWindow* parent);
+    static wxButton* CreateDecimalButton(wxWindow* parent);
+    static wxButton* CreateUnaryOperationButton(wxWindow* parent, const wxString& label);
 };
 
